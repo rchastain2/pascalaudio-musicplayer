e22d8461e7aa671ab2a2e2cfbafefcac776d834b
Roland Chastain <roland.chastain@gmx.fr>
2026-06-17 22:03:32 +0200

    Add OGG player

---
+++ player-ogg/musicplayer.pas
@@ -0,0 +1,21 @@
+
+program musicplayer;
+
+{$IFDEF FPC}{$MODE objfpc}{$H+}{$ENDIF}
+{$IFDEF FPC}
+{$IFDEF mswindows}{$APPTYPE gui}{$ENDIF}
+{$ENDIF}
+
+uses
+{$IFDEF FPC}{$IFDEF unix}
+  cthreads,
+{$ENDIF}{$ENDIF}
+  msegui,
+  main,
+  log;
+
+begin
+  logln('PascalAudio Music Player ' + {$I version} + ' (' + {$I %DATE%} + ', ' + {$I %TIME%} + ', FPC ' + {$I %FPCVERSION%} + ', ' + {$I %FPCTARGETOS%} + ')', TRUE);
+  application.createform(tmainfo, mainfo);
+  application.run;
+end.
