e5742aa3887f3e7117525b45db7dd6f6f312a1d1
Roland Chastain <roland.chastain@gmx.fr>
2026-06-23 18:50:28 +0200

    Use Oxanium font

--- player/player.pas
+++ player/player.pas
@@ -11,11 +11,15 @@ uses
   cthreads,
 {$ENDIF}{$ENDIF}
   msegui,
+  msefont,
+  msegraphics,
   main,
   log;
 
 begin
   logln('PascalAudio Music Player ' + {$I version} + ' (' + {$I %DATE%} + ', ' + {$I %TIME%} + ', FPC ' + {$I %FPCVERSION%} + ', ' + {$I %FPCTARGETOS%} + ')', TRUE);
+  registerfontalias('stf_fancy', 'Oxanium', fam_overwrite, 26);
+  registerfontalias('stf_default', 'Oxanium', fam_overwrite, 14);
   application.createform(tmainfo, mainfo);
   application.run;
 end.
